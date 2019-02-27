44aacd3c03a98dbd4fac4089872613af2778d6c0
li houlei <kourai.ri@seiko-sol.co.jp>
2019-02-27 16:38:52 +0900

    挂载axios

--- vue-unit-test-with-jest/src/main.js
+++ vue-unit-test-with-jest/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
+
+Vue.prototype.$axios = Axios
 
 Vue.config.productionTip = false
 
