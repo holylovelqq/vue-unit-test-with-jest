76c177f7efd0c137af982a18f9d240fe352c19c6
li houlei <kourai.ri@seiko-sol.co.jp>
2019-03-27 14:01:24 +0900

    增加一个外部方法完善测试示例

--- vue-unit-test-with-jest/src/assets/js/CommonMethods.js
+++ vue-unit-test-with-jest/src/assets/js/CommonMethods.js
@@ -1,20 +1,3 @@
-import Vue from 'vue'
-import store from '@/store.js'
-
-/**
- * 格式化时间
- * @params date :utctime-'YYYY-MM-DDTHH:mm:ss.SSSZ'
- * @returns 指定时区的时间
- */
-export function formatDatetime (date) {
-  var m = Vue.prototype.$moment.utc(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ')
-  // タイムゾーンは分で指定する
-  // 日本：9*60=54min
-  // https://stackoverflow.com/questions/32878197/updating-time-offset-with-moment-utcoffset
-
-  return m.utcOffset(store.state.TZDeal.TZone).format('YYYY/MM/DD HH:mm')
-}
-
 /**
  * 获取当前视口的宽度，判断默认样式
  * @returns true<1025<=false
