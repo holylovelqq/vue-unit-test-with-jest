4b62df942f9372ebfb8b8d8217c02bd8e6dc6031
li houlei <kourai.ri@seiko-sol.co.jp>
2019-03-27 13:26:34 +0900

    添加vuex测试组件

---
+++ vue-unit-test-with-jest/src/components/VuexTest.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- vuex的单元测试例 -->
+    <div class="example vuex">
+      <h2 class="example_title">vuex的单元测试例</h2>
+      <app-button size="lg" @click="goVIPs">更改vuex内的变量值</app-button>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+</style>
