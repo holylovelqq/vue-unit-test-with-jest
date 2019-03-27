424969634644c7e76ee653e165dd443a15582d45
li houlei <kourai.ri@seiko-sol.co.jp>
2019-03-27 13:25:58 +0900

    添加router测试组件

---
+++ vue-unit-test-with-jest/src/components/RouterTest.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- 路由跳转的单元测试例 -->
+    <div class="example router">
+      <h2 class="example_title">路由跳转的单元测试例</h2>
+      <app-button size="lg" @click="goVIPs">跳转到VIP用户页面</app-button>
+    </div>
+</template>
+<script>
+export default {
+  methods: {
+    goVIPs () {
+      this.$router.push({
+        path: '/vips'
+      })
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
