a4d51aa1440c8cadc6f313ed0abd9846e6faf959
li houlei <kourai.ri@seiko-sol.co.jp>
2019-03-27 13:25:36 +0900

    添加filter测试组件

---
+++ vue-unit-test-with-jest/src/components/FilterTest.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- filter的单元测试例 -->
+    <div class="example filter">
+      <h2 class="example_title">filter的单元测试例</h2>
+      <el-input class="input" v-model="userInput" placeholder="输入长度大于7的内容，下方将显示过滤后内容"></el-input>
+      <div v-show="toggleShow" class="text format">{{userInput | formatText}}</div>
+      <div v-show="!toggleShow" class="text noformat">{{userInput}}</div>
+      <app-button @click="changeShow">切换显示方式</app-button>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      userInput: '',
+      toggleShow: true
+    }
+  },
+  filters: {
+    formatText: function (value) {
+      // console.log(value)
+      if (!value) return ''
+      if (value.length > 7) {
+        let frontVal = value.slice(0, 2)
+        let endVal = value.slice(value.length - 2, value.length)
+        return frontVal + '...' + endVal
+      } else {
+        return value
+      }
+    }
+  },
+  methods: {
+    changeShow () {
+      this.toggleShow = !this.toggleShow
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
