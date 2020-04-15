3ff0ea07d95ebfcb40e950d4ba8da486348457a9
holy <510308301@qq.com>
2020-04-15 13:13:03 +0900

    :+1: vuetify 自定义

--- 02-test-advanced/src/main.ts
+++ 02-test-advanced/src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
