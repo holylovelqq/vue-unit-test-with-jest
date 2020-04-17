9e151fff80f7e2ebf76b0afc3addb9ca94edca89
holy <510308301@qq.com>
2020-04-17 19:33:36 +0900

    :fire: 恢复默认设置

--- 02-test-advanced/src/plugins/axios.js
+++ 02-test-advanced/src/plugins/axios.js
@@ -43,11 +43,6 @@ Plugin.install = function (Vue, options) {
   Vue.axios = _axios
   window.axios = _axios
   Object.defineProperties(Vue.prototype, {
-    axios: {
-      get () {
-        return _axios
-      }
-    },
     $axios: {
       get () {
         return _axios
