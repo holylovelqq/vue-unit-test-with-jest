341a5c00bc4bb5ac58dbd6fdac395f9f408b5570
holy <510308301@qq.com>
2020-04-20 15:32:13 +0900

    :+1: 使用包含slot的组件，并添加自定义内容

--- 02-test-advanced/src/views/Test.vue
+++ 02-test-advanced/src/views/Test.vue
@@ -6,6 +6,12 @@
     <count-test class="mt-4"></count-test>
     <v-divider></v-divider>
     <picture-test class="my-4"></picture-test>
+    <v-divider></v-divider>
+    <slot-test class="ma-4">
+      <template #default>
+        slot测试自定义内容
+      </template>
+    </slot-test>
   </div>
 
 </template>
@@ -15,13 +21,15 @@ import Vue from 'vue'
 import tableTest from '@/components/Table/Table.vue'
 import countTest from '@/components/Count/Count.vue'
 import pictureTest from '@/components/Picture/Picture.vue'
+import slotTest from '@/components/SlotTest/SlotTest.vue'
 
 export default Vue.extend({
   name: 'Test',
   components: {
     tableTest,
     countTest,
-    pictureTest
+    pictureTest,
+    slotTest
   }
 })
 </script>
